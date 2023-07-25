2d8c6d7638f17c46888af15eca338d678f124e9a
Abdullah-167 <abdullahpadhana503@gmail.com>
2023-07-25 21:11:02 +0500

    commit done

---
+++ src/Components/Views/Home/CheckDish.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+const CheckDish = () => {
+    return (
+        <div className='dish-bg py-40'>
+            <div className='container'>
+                <div className='max-w-[470px]'>
+                    <h2 className='text-[50px] font-secondary leading-[50px] relative pb-7'>
+                        Check out my <span className=' relative'>  newest <span className='border-b-[3px] border-b-[#fee86d] absolute top-10 w-36 h-2 -right-[2px]'></span> </span>  vegan recipes books
+                    </h2>
+                    <p className='text-lg text-gray-500 pb-8'>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   </p>
+                    <div className='flex gap-5'>
+                        <Image src={'/dish1.png'} alt='food' width={130} height={130} />
+                        <Image src={'/dish2.png'} alt='food' width={130} height={130} />
+                        <Image src={'/dish3.png'} alt='food' width={130} height={130} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CheckDish
--- src/Components/Views/Home/index.jsx
+++ src/Components/Views/Home/index.jsx
@@ -1,4 +1,5 @@
 import Cards from './Cards'
+import CheckDish from './CheckDish'
 import Header from './Header'
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <>
             <Header />
             <Cards />
+            <CheckDish />
         </>
     )
 }
